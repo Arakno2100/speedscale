cf6f6302c9c6400635e7833528e8de55c88c3f71
Arakno2100 <162506213+Arakno2100@users.noreply.github.com>
2025-01-03 14:40:23 +0100

    feat: created registration logic

---
+++ speedscale/speedscale-war/web/common/registration.jsp
@@ -0,0 +1,62 @@
+<%@ page import="java.util.List" %>
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrazione</title>
+    <script src="../scripts/validate.js"></script>
+</head>
+<body>
+<jsp:include page="/jsp/header.jsp"/>
+<div class="container">
+    <h2>Registrazione</h2>
+    <img style="width: 30%; margin: 0 15%;" src="${pageContext.request.contextPath}/images/logo.png" alt="Logo">
+    <form id="regForm" action="${pageContext.request.contextPath}/common/RegistrationServlet" method="post">
+        <div class="form-group">
+            <div class="form-group">
+                <label for="nome">Nome</label>
+                <input type="text" id="nome" name="nome" required  onblur="validateNome('regForm')">
+            </div>
+            <span id="errorName"></span>
+            <div class="form-group">
+                <label for="cognome">Cognome</label>
+                <input type="text" id="cognome" name="cognome" required onblur="validateCognome('regForm')">
+            </div>
+            <span id="errorLastname"></span>            
+            <div class="form-group">
+                <label for="dataNascita">Data di Nascita</label>
+                <input type="date" id="dataNascita" name="dataNascita" required>
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="text" id="email" name="email" required onblur="validateMail('regForm')">
+            </div>
+            <span id="errorEmail"></span>
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" required onfocus="validate()">
+        </div>
+        <div class="form-group">
+            <button id="buttonReg" type="submit" onmouseover="validateReg('regForm' ,'buttonReg' )">Continua</button>
+        </div>
+        <a  href="index.jsp">Home</a>
+        <a  style="margin-right: 0" href="common/login.jsp">Login</a>
+    </form>
+    <div id="errorMessages">
+        <%
+            List<String> errors = (List<String>) request.getAttribute("errors");
+            if (errors != null && !errors.isEmpty()) {
+                for (String error : errors) {
+        %>
+        <p class="error-message"><%= error %></p>
+        <%
+                }
+            }
+        %>
+    </div>
+</div>
+<jsp:include page="/jsp/footer.jsp"/>
+</body>
+</html>
---
+++ speedscale/speedscale-war/web/scripts/validate.js
@@ -0,0 +1,195 @@
+const nameOrLastnamePattern = /^[A-Za-z]+$/;
+const emailPattern = /^\S+@\S+\.\S+$/;
+const phonePattern = /^([0-9]{10})$/;
+const addressPattern = /^(Via|Corso|Piazza|Viale|Largo|Borgo)\s+[A-Za-zàèéìòùÀÈÉÌÒÙ]+\s*([A-Za-zàèéìòùÀÈÉÌÒÙ]*\s*)*$/;
+const capPattern = /^([0-9]{5})$/
+const provPattern = /^[A-Z]{2}$/
+
+const nameErrorMessage = "Il nome deve avere solo lettere";
+const lastnameErrorMessage = "Il cognome deve avere solo lettere";
+const cittaError = "La citt&agrave deve avere solo lettere"
+const nazioneError = "La nazione deve avere solo lettere"
+const emailErrorMessage = "La mail dovrebbe essere del tipo username@domain.ext";
+const numberErrorMessage = "Un numero valido ha 10 cifre";
+const addressError = "Indirizzo non valido deve Iniziare con Via/Corso... ";
+const capError = "Un cap è fatto solo di 5 cifre";
+const provError = "Un provincia &egrave formata solo da due lettere maiuscole come NA";
+let count = 1;
+
+function validateFormElem(formElem, pattern, span, message) {
+    if(formElem.value.match(pattern)){
+        formElem.classList.remove("error");
+        span.style.color = "black";
+        span.innerHTML = "";
+        return true;
+    }
+    formElem.classList.add("error");
+    span.innerHTML = message;
+    span.style.color = "red";
+    return false;
+}
+function validateAllAddress(str , btn){
+    let valid =  validateAddress(str , btn) && validateCitta(str ,btn) && validateProv(str, btn) && validateCap(str, btn) && validateNaz(str,btn);
+    const butt = document.getElementById(btn);
+    butt.disabled = !valid;
+}
+function validateReg(str, btn){
+    let valid = validateNome(str) && validateCognome(str) && validateMail(str);
+    const butt = document.getElementById(btn);
+    butt.disabled = !valid;
+    console.log("Festa");
+}
+function  validateAllName(str  , btn){
+    let valid = validateNome(str) && validateCognome(str) && validatePhone(str);
+    const butt = document.getElementById(btn);
+    butt.disabled = !valid;
+}
+function validateNome(str) {
+    let valid = true;
+    let form = document.getElementById(str);
+
+    let spanName = document.getElementById("errorName");
+    if (!validateFormElem(form.nome, nameOrLastnamePattern, spanName, nameErrorMessage)) {
+        valid = false;
+    }
+    return valid;
+}
+function validateCognome(str) {
+    let valid = true;
+    let form = document.getElementById(str);
+
+    let spanLastname = document.getElementById("errorLastname");
+    if (!validateFormElem(form.cognome, nameOrLastnamePattern, spanLastname, lastnameErrorMessage)) {
+        valid = false;
+    }
+    return valid;
+}
+function validateMail(str) {
+    let valid = true;
+    let form = document.getElementById(str);
+
+    let spanEmail = document.getElementById("errorEmail");
+    if (!validateFormElem(form.email, emailPattern, spanEmail, emailErrorMessage)){
+        valid = false;
+    }
+    return valid;
+}
+function validatePhone(str) {
+    let valid = true;
+    let form = document.getElementById(str);
+    let spanPhone = document.getElementById("errorPhone");
+    let phoneElement = document.getElementById("phone");
+    let phoneValue = phoneElement.value.toString();
+
+    // Verifica se il campo del telefono è vuoto o se non passa la validazione del pattern
+    if (phoneValue!=="" && phoneValue !== "null" && !validateFormElem(phoneElement, phonePattern, spanPhone, numberErrorMessage)) {
+        valid = false;
+    }
+    return valid;
+}
+function validateAddress(str) {
+    let valid = true;
+    let form = document.getElementById(str);
+
+    let spanName = document.getElementById("errorAddress");
+    if (!validateFormElem(form.via, addressPattern, spanName, addressError)) {
+        valid = false;
+    }
+    return valid;
+}
+function validateCitta(str) {
+    let valid = true;
+    let form = document.getElementById(str);
+
+    let spanName = document.getElementById("errorCitta");
+    if (!validateFormElem(form.citta, nameOrLastnamePattern, spanName, cittaError)) {
+        valid = false;
+    }
+    return valid;
+}
+function validateProv(str) {
+    let valid = true;
+    let form = document.getElementById(str);
+
+
+    let spanName = document.getElementById("errorProv");
+    if (!validateFormElem(form.provincia, provPattern, spanName, provError)) {
+        valid = false;
+    }
+
+    return valid;
+}
+function validateCap(str) {
+    let valid = true;
+    let form = document.getElementById(str);
+
+    let spanName = document.getElementById("errorCap");
+    if (!validateFormElem(form.cap, capPattern, spanName, capError)) {
+        valid = false;
+
+    }
+
+    return valid;
+}
+function validateNaz(str) {
+    let valid = true;
+    let form = document.getElementById(str);
+
+
+    let spanName = document.getElementById("errorNaz");
+    if (!validateFormElem(form.nazione, nameOrLastnamePattern, spanName, nazioneError)) {
+        valid = false;
+
+    }
+    return valid;
+
+}
+function  validatePrice(id , spanName){
+
+
+    const input = document.getElementById(id).value;
+    if(Number.parseFloat(input) <=  0)
+        document.getElementById(spanName).innerHTML = "Inserisci un numero maggiore di 0"
+    else
+        document.getElementById(spanName).innerHTML = ""
+
+
+    return Number.parseFloat(input) <=  0;
+
+}
+function validateQuantPrice(id1 , id2 , btn )
+{
+
+    const input1 = document.getElementById(id1).value;
+    const input2 = document.getElementById(id2).value;
+    const butt = document.getElementById(btn)
+    let val = Number.parseFloat(input1) <=  0 || Number.parseFloat(input2) <=  0;
+    butt.disabled = val;
+
+
+}
+function removeSpecial (){
+    document.getElementById("nome").value = escapeHtml(   document.getElementById("nome").value );
+    document.getElementById("descrizione").value = escapeHtml(   document.getElementById("descrizione").value );
+    document.getElementById("isbn").value = escapeHtml(   document.getElementById("isbn").value );
+    document.getElementById("prezzo").value = escapeHtml(   document.getElementById("prezzo").value );
+    document.getElementById("quantita").value = escapeHtml(   document.getElementById("quantita").value );
+
+}
+function escapeHtml(str) {
+    const entityMap = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#39;',
+        '/': '&#x2F;',
+        '`': '&#x60;',
+        '=': '&#x3D;'
+    };
+
+    return String(str).replace(/[&<>"'`=\/]/g, function (s) {
+        return entityMap[s];
+    });
+}
+
